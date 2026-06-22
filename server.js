8afc2fd8331c1cbaf9b8dfa50bbafdab58f8ce5f
RaunakKumar-byte <singhraunakkumar524@gmail.com>
2026-06-22 22:27:27 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express=require("express");
+const mongoose=require("mongoose");
+
+const app=express();
+
+
+const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
+
+main.then(()=>{
+    console.log("db connected");
+})
+.catch((err)=>{
+    console.log(err);
+})
+async function main(){
+    await mongoose.connect(MONGO_URL);
+}
+
+app.get("/",(res,req)=>{
+    res.send("Hi i am root");
+})
+
+app.listen(8080,()=>{
+        console.log(`server is runing on ${8080}`);
+})
